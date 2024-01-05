e59458df88b5e41ce94b440105d265e4bf0d06cf
Carlos Vieira <carlos.vieira@sellwerk.de>
2024-01-05 15:50:59 +0000

    add two layouts

---
+++ components/GlobalHeader.vue
@@ -0,0 +1,1 @@
+<template>carlitos 2</template>
---
+++ layouts/global.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <GlobalHeader />
+    <slot />
+  </div>
+</template>
+<script setup>
+import GlobalHeader from "~/components/GlobalHeader.vue";
+</script>
